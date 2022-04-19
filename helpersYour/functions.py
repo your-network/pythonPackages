c3b903c94f1a60713909e38b9fdefcd5b7290b11
thijmen francken <thijmenfrancken@gmail.com>
2022-04-19 13:48:56 +0200

    helpers additional functions

--- helpersYour/functions.py
+++ helpersYour/functions.py
@@ -12,3 +12,28 @@ def get_digits(string):
 def remove_nulls(d):
     none_filtered = {k: v for k, v in d.items() if v is not None}
     return {k: v for k, v in none_filtered.items() if v != ''}
+
+def remove_dic_none_values(dic):
+    remove_fields = []
+    for key,value in dic.items():
+        if value is None and key is not None:
+            remove_fields.append(key)
+    final_dic = remove_dic_key(dic,remove_fields)
+    return final_dic
+
+def remove_dic_key(dic,keys):
+    for remove in keys:
+        try:
+            del dic[remove]
+        except:
+            continue
+    return dic
+
+def rename_dic_key(dic={},naming=[]):
+    for row in naming:
+        old = row.get('old',None)
+        new = row.get('new',None)
+        if old:
+            dic[new] = dic.pop(old)
+    return dic
+
