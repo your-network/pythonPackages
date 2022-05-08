440038fe6d59cc79becdc9d4ecd04481704919cc
thijmen francken <thijmenfrancken@gmail.com>
2022-05-08 14:59:33 +0200

    selenium argumnet disable extensions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonPackages",
-    version='0.0.8',
+    version='0.0.9',
     author='Thijmen Francken',
     author_email='thijmenfrancken@gmail.com',
     description='All packages used for YOUR development',
