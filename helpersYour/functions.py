324bb75e8e34a10a27377604db0e462322797fc4
thijmen francken <thijmenfrancken@gmail.com>
2022-05-18 15:14:57 +0200

    flatten json in python packages

--- helpersYour/functions.py
+++ helpersYour/functions.py
@@ -91,3 +91,21 @@ def image_details(image_url):
     Na = np.array(im).astype(np.uint16)
     sha256 = hashlib.sha256(Na.tobytes()).hexdigest()
     return w, h, sha256
+
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '.')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '.')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonPackages",
-    version='0.1.3',
+    version='0.1.4',
     author='Thijmen Francken',
     author_email='thijmenfrancken@gmail.com',
     description='All packages used for YOUR development',
