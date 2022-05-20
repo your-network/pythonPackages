d30896d6bff4f65d8aa2fad2e63f15e8cf9d0a35
thijmen francken <thijmenfrancken@gmail.com>
2022-05-20 16:57:58 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonPackages",
-    version='0.1.4',
+    version='0.1.5',
     author='Thijmen Francken',
     author_email='thijmen@your.io',
     description='All packages used for YOUR development',
