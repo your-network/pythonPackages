c8a7f6d4787e56d148f3b70ebdf3c92ee378b38f
thijmen francken <thijmenfrancken@gmail.com>
2022-05-31 09:36:04 +0200

    update sqlalchemy for pool closing and auto commits

--- dbYour/YourProcessingInit.py
+++ dbYour/YourProcessingInit.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import sessionmaker
 class ProcessingDB:
 
     def __init__(self, SQLALCHEMY_DATABASE_URI):
-        self.engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_recycle=True)
+        self.engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_recycle=3600)
 
-        self.Session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        self.Session = sessionmaker(autocommit=True, autoflush=True, bind=self.engine)
 
     def startSession(self):
         from .YourProcessingModels import Base
@@ -19,4 +19,5 @@ class ProcessingDB:
 
     def closeSession(self):
         self.session.close()
-        self.engine.dispose()
+
+        self.engine.close()
