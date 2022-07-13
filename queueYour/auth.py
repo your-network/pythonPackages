6c1868dacaf992e3f495ab22467ca74d9807699e
thijmen francken <thijmenfrancken@gmail.com>
2022-07-13 16:49:47 +0200

    batch message function pub/sub

--- queueYour/auth.py
+++ queueYour/auth.py
@@ -34,3 +34,23 @@ class QueueAuth:
         publisher = pubsub_v1.PublisherClient(credentials=credentials_pub)
 
         return publisher
+
+    def getQueueBatchPublisher(self):
+        batch_settings = pubsub_v1.types.BatchSettings(
+            max_messages=100,  # default 100
+            max_bytes=1024,  # default 1 MB
+            max_latency=1,  # default 10 ms
+        )
+
+        publisher_audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+
+        credentials = jwt.Credentials.from_service_account_info(
+            self.service_account_info, audience=publisher_audience
+        )
+
+        credentials_pub = credentials.with_claims(audience=publisher_audience)
+
+        batch_publisher = pubsub_v1.PublisherClient(credentials=credentials_pub,
+                                              batch_settings=batch_settings)
+
+        return batch_publisher
--- queueYour/publish.py
+++ queueYour/publish.py
@@ -1,5 +1,12 @@
 import json
 import os
+from concurrent import futures
+from google.cloud import pubsub_v1
+
+# Resolve the publish future in a separate thread.
+def callback(future: pubsub_v1.publisher.futures.Future) -> None:
+    message_id = future.result()
+    print(message_id)
 
 def publishTopicMessage(publisher, topic_name, data):
     topic = f"{os.environ['TOPIC_CONSTRUCT']}{topic_name}"
@@ -7,3 +14,17 @@ def publishTopicMessage(publisher, topic_name, data):
     future = publisher.publish(topic, json.dumps(data).encode('utf-8'), spam='eggs')
 
     print(f"result: {future.result()}")
+
+def publishTopicBatchMessages(batch_publisher, topic_name, batch_data):
+
+    topic = f"{os.environ['TOPIC_CONSTRUCT']}{topic_name}"
+
+    publish_futures = []
+
+    for data in batch_data:
+        publish_future = batch_publisher.publish(topic, json.dumps(data).encode('utf-8'))
+        publish_future.add_done_callback(callback)
+        publish_futures.append(publish_future)
+
+    futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
+
