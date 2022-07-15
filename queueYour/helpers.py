def69830e1f442cbee9bd24bb21e973c2dc46246
thijmen francken <thijmenfrancken@gmail.com>
2022-07-15 15:08:21 +0200

    acknowledge messages for pub/sub queue

--- queueYour/helpers.py
+++ queueYour/helpers.py
@@ -19,3 +19,13 @@ class Utils:
             return decoded_data
         except Exception as e:
             raise Exception('error converting to UTF')
+
+
+def ack_messages(subscriber, subscription_path, message_ack_ids):
+    if len(message_ack_ids) > 0:
+        subscriber.acknowledge(
+            request={
+                "subscription": subscription_path,
+                "ack_ids": message_ack_ids,
+            }
+        )
