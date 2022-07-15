bfe958cbaa461b332b8f7a4b03cbedd817f9f500
thijmen francken <thijmenfrancken@gmail.com>
2022-07-15 14:30:11 +0200

    Queue code optimization

--- queueYour/auth.py
+++ queueYour/auth.py
@@ -7,50 +7,34 @@ class QueueAuth:
 
     def __init__(self, service_account_info, project_id):
         self.service_account_info = service_account_info
+        self.project_id = self.project_id
         os.environ["TOPIC_CONSTRUCT"] = f"projects/{project_id}/topics/"
         os.environ["SUB_CONSTRUCT"] = f"projects/{project_id}/subscriptions/"
 
-    def getQueueSubscriber(self):
 
-        audience = "https://pubsub.googleapis.com/google.pubsub.v1.Subscriber"
-
-        credentials = jwt.Credentials.from_service_account_info(
-            self.service_account_info, audience=audience
+    def connectSubscribeQueue(self, subscriber_name):
+        subscriber = pubsub_v1.SubscriberClient.from_service_account_json(self.service_account_info)
+        subscription_path = subscriber.subscription_path(
+            self.project_id,
+            subscriber_name
         )
 
-        subscriber = pubsub_v1.SubscriberClient(credentials=credentials)
+        return subscriber, subscription_path
 
-        return subscriber
-
-    def getQueuePublisher(self):
-        publisher_audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
-
-        credentials = jwt.Credentials.from_service_account_info(
-            self.service_account_info, audience=publisher_audience
-        )
-
-        credentials_pub = credentials.with_claims(audience=publisher_audience)
-
-        publisher = pubsub_v1.PublisherClient(credentials=credentials_pub)
+    def connectPublisherQueue(self):
+        publisher = pubsub_v1.PublisherClient.from_service_account_json(self.service_account_info)
 
         return publisher
 
-    def getQueueBatchPublisher(self):
+    def connectBatchPublisherQueue(self):
         batch_settings = pubsub_v1.types.BatchSettings(
             max_messages=100,  # default 100
             max_bytes=1024,  # default 1 MB
             max_latency=1,  # default 10 ms
         )
 
-        publisher_audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+        publisher = pubsub_v1.PublisherClient.from_service_account_json(self.service_account_info,
+                                                                        batch_settings=batch_settings)
 
-        credentials = jwt.Credentials.from_service_account_info(
-            self.service_account_info, audience=publisher_audience
-        )
+        return publisher
 
-        credentials_pub = credentials.with_claims(audience=publisher_audience)
-
-        batch_publisher = pubsub_v1.PublisherClient(credentials=credentials_pub,
-                                              batch_settings=batch_settings)
-
-        return batch_publisher
