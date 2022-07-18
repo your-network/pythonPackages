026730a706b1edeb01e5bceaa053e7d8b03a2f78
thijmen francken <thijmenfrancken@gmail.com>
2022-07-18 16:24:18 +0200

    change queue settings batch printing

--- queueYour/publish.py
+++ queueYour/publish.py
@@ -6,7 +6,7 @@ from google.cloud import pubsub_v1
 # Resolve the publish future in a separate thread.
 def callback(future: pubsub_v1.publisher.futures.Future) -> None:
     message_id = future.result()
-    print(message_id)
+    # print(message_id)
 
 def publishTopicMessage(publisher, topic_name, data):
     topic = f"{os.environ['TOPIC_CONSTRUCT']}{topic_name}"
@@ -26,4 +26,6 @@ def publishTopicBatchMessages(batch_publisher, topic_name, batch_data):
             publish_futures.append(publish_future)
 
         futures.wait(publish_futures, return_when=futures.ALL_COMPLETED)
+
+        print(f"All messages published. Number messages: {len(batch_data)}")
 
