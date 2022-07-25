81219cdc89d621fe6a2e9ef6dc33b8dbee513c4b
thijmen francken <thijmenfrancken@gmail.com>
2022-07-25 10:25:30 +0200

    testing lookup category ids

---
+++ apiYour/settingsApi.py
@@ -0,0 +1,11 @@
+SOURCE_IDS = [{'name': 'Internal', 'id': '1'},
+              {'name': 'Icecat', 'id': '2'},
+              {'name': 'Bol', 'id': '3'},
+              {'name': 'Amazon', 'id': '4'},
+              {'name': 'Opensea', 'id': '5'},
+              {'name': 'Decentraland', 'id': '6'},
+              {'name': 'Sandbox', 'id': '7'}]
+
+PURPOSE_IDS = [{'name': 'Taxonomy', 'id': '1'},
+               {'name': 'AttributeGroup', 'id': '2'},
+               {'name': 'NFTCollection', 'id': '3'}]
