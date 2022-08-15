14fd9aeeadc07d3a12c6c23c00f72c2ac6f0e407
thijmen francken <thijmenfrancken@gmail.com>
2022-08-15 15:46:15 +0200

    changing media upload

--- amazonYour/media.py
+++ amazonYour/media.py
@@ -5,11 +5,15 @@ import requests
 
 def mediaUrlS3BucketUpload(amazonS3Client, media_url, internal_path):
     response = requests.get(media_url)
-    bytes_content = BytesIO(response.content)
-    content_type = response.headers['Content-Type']
-    ## upload details
-    return uploadBytesMedia(amazonS3Client,
-                    "yourcontent-dev",
-                    bytes_content,
-                    internal_path,
-                    content_type)
+    if response.status_code == 200:
+        bytes_content = BytesIO(response.content)
+        content_type = response.headers['Content-Type']
+        ## upload details
+        return uploadBytesMedia(amazonS3Client,
+                        "yourcontent-dev",
+                        bytes_content,
+                        internal_path,
+                        content_type)
+
+    else:
+        return False
