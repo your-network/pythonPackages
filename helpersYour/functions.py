d0b68f09649a56769ffe332c8a5c8677f3b37e26
thijmen francken <thijmenfrancken@gmail.com>
2022-09-21 14:22:59 +0200

    chunkIt

--- helpersYour/functions.py
+++ helpersYour/functions.py
@@ -126,6 +126,10 @@ def splitList(list_a, chunk_size):
   for i in range(0, len(list_a), chunk_size):
     yield list_a[i:i + chunk_size]
 
+def chunkIt(my_list, n_chunks):
+    chunks = np.array_split(my_list, n_chunks)
+    return chunks
+
 class PathResolver:
 
     @staticmethod
