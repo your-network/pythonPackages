436c543966bbae927d2af9f80d9df3a744ebc38b
thijmen francken <thijmenfrancken@gmail.com>
2022-09-29 14:12:02 +0200

    helpers connection

--- apiYour/helpers.py
+++ apiYour/helpers.py
@@ -8,7 +8,7 @@ def fillterCategoryPurpose(categories: list,
 
     return filter_list
 
-def getApiRequestsSession(endpoint: str = None) -> object:
+def getApiRequestsSession(endpoints: list = ["https://"]) -> object:
     import requests
     from requests.adapters import HTTPAdapter
     # To make sure we can use the requests session in the threadpool we
@@ -16,11 +16,10 @@ def getApiRequestsSession(endpoint: str = None) -> object:
     # will raise when it runs out of connections and the threads will be
     # terminated.
     requests_session = requests.Session()
-    mount_url = "https://"
-    if endpoint:
-        mount_url = endpoint
-    requests_session.mount(mount_url, HTTPAdapter(
-        pool_connections=10, pool_maxsize=1000, max_retries=5,
-        pool_block=True))
+
+    for endpoint in endpoints:
+        requests_session.mount(endpoint, HTTPAdapter(
+            pool_connections=10, pool_maxsize=1000, max_retries=5,
+            pool_block=True))
 
     return requests_session
