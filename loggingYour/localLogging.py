9f2901bfb36b6560377125856106402ef22e16a7
thijmen francken <thijmenfrancken@gmail.com>
2022-11-23 10:20:59 +0100

    logging changes from .log to .json for better formatting

--- loggingYour/localLogging.py
+++ loggingYour/localLogging.py
@@ -1,5 +1,6 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from loggingYour.formatter import JsonFormatter
 
 class LocalLogger:
 
@@ -10,27 +11,53 @@ class LocalLogger:
         self.local_logger = logging.getLogger(logger_name)
         self.local_logger.setLevel(logging.DEBUG)
         # create file handler which logs even debug messages
-        ih_handler = TimedRotatingFileHandler(filename=f"{log_path}/info.log", when='D',
-                                              interval=1, backupCount=5, encoding='utf-8', delay=False)
+        ih_handler = TimedRotatingFileHandler(filename=f"{log_path}/info.json",
+                                              when='D',
+                                              interval=1,
+                                              backupCount=5,
+                                              encoding='utf-8',
+                                              delay=False)
         ih_handler.setLevel(logging.INFO)
         # create file handler which logs even debug messages
-        fh_handler = TimedRotatingFileHandler(filename=f"{log_path}/debug.log", when='D',
-                                              interval=1, backupCount=5, encoding='utf-8', delay=False)
+        fh_handler = TimedRotatingFileHandler(filename=f"{log_path}/debug.json",
+                                              when='D',
+                                              interval=1,
+                                              backupCount=5,
+                                              encoding='utf-8',
+                                              delay=False)
         fh_handler.setLevel(logging.DEBUG)
-        # create console handler with a error log level
-        ch_handler = TimedRotatingFileHandler(filename=f"{log_path}/error.log", when='D',
-                                              interval=1, backupCount=5, encoding='utf-8', delay=False)
+        # create console handler with an error log level
+        ch_handler = TimedRotatingFileHandler(filename=f"{log_path}/error.json",
+                                              when='D',
+                                              interval=1,
+                                              backupCount=5,
+                                              encoding='utf-8',
+                                              delay=False)
         ch_handler.setLevel(logging.ERROR)
         # create console handler with a higher log level
-        wh_handler = TimedRotatingFileHandler(filename=f"{log_path}/warning.log", when='D',
-                                              interval=1, backupCount=5, encoding='utf-8', delay=False)
+        wh_handler = TimedRotatingFileHandler(filename=f"{log_path}/warning.json",
+                                              when='D',
+                                              interval=1,
+                                              backupCount=5,
+                                              encoding='utf-8',
+                                              delay=False)
         wh_handler.setLevel(logging.WARNING)
         # create formatter and add it to the handlers
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh_handler.setFormatter(formatter)
-        ch_handler.setFormatter(formatter)
-        ih_handler.setFormatter(formatter)
-        wh_handler.setFormatter(formatter)
+        json_formatter = JsonFormatter({"level": "levelname",
+                                        "message": "message",
+                                        "data": "data",
+                                        "error_code": "error_code",
+                                        "loggerName": "name",
+                                        "processName": "processName",
+                                        "processID": "process",
+                                        "threadName": "threadName",
+                                        "threadID": "thread",
+                                        "timestamp": "asctime"})
+        # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh_handler.setFormatter(json_formatter)
+        ch_handler.setFormatter(json_formatter)
+        ih_handler.setFormatter(json_formatter)
+        wh_handler.setFormatter(json_formatter)
         # add the handlers to the logger
         self.local_logger.addHandler(fh_handler)
         self.local_logger.addHandler(ch_handler)
