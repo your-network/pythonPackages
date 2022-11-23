93b893a44becc8178b282758e0ab662df5787594
thijmen francken <thijmenfrancken@gmail.com>
2022-11-23 16:38:27 +0100

    logging data keyerror fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pythonPackages",
-    version='0.6.1.0',
+    version='0.6.1.1',
     author='Thijmen Francken',
     author_email='thijmen@your.io',
     description='All packages used for YOUR development',
