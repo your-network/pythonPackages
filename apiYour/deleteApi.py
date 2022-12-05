1b985871ab60d4bb510597edb28d6303ccafa992
thijmen francken <thijmenfrancken@gmail.com>
2022-12-05 12:01:17 +0100

    remove product media file

--- apiYour/deleteApi.py
+++ apiYour/deleteApi.py
@@ -2,9 +2,54 @@ import os
 import requests
 import json
 from datetime import datetime
-from typing import Tuple
 from loggingYour.messageHandler import messageHandler
 from apiYour.settingsApi import PRODUCTION_ADDRESS, DEVELOPMENT_ADDRESS
+
+def removeProductMedia(logger:object,
+                       connection: object,
+                       media: str,
+                       productId: int = None,
+                       environment: str = "production") -> dict:
+
+    msg_handler = messageHandler(logger=logger, level="DEBUG",
+                                 labels={'function': 'removeProductMedia',
+                                         'endpoint': '/Product/{productId}/Media/{media}'})
+    ## construct request
+    base_params = {}
+    if environment == "production":
+        request_url = f"{PRODUCTION_ADDRESS}/Product/{productId}/Media/{media}"
+    elif environment == "development":
+        request_url = f"{DEVELOPMENT_ADDRESS}/Product/{productId}/Media/{media}"
+
+    ## logging
+    msg_handler.logStruct(
+        topic=f"removeProductMedia: delete media from product",
+        data=base_params)
+
+    ## request variables
+    try:
+        ## process request from connection pool
+        r = connection.request(method="DELETE",
+                               url=request_url,
+                               headers={'Authorization': 'Bearer ' + os.environ["YOUR_API_TOKEN"],
+                                        'Content-Type': 'application/json'})
+
+        response_code = r.status
+        response_text = r.data
+        r.close()
+        if response_code == 200:
+            return True
+        else:
+            return False
+
+
+    except Exception as e:
+        msg_handler.logStruct(topic="removeProductMedia: Error removing media",
+                              error_message=str(e))
+
+    return False
+
+
 
 def removeCategoryMedia(logger:object,
                         media: str,
