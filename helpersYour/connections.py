579fe57c071fd8dae577279ed50aac975982e12a
thijmen francken <thijmenfrancken@gmail.com>
2023-01-02 10:35:01 +0100

    connection pool timeout increase

--- helpersYour/connections.py
+++ helpersYour/connections.py
@@ -8,7 +8,7 @@ class HTTPConnections:
 
         self.http_pool = urllib3.PoolManager(num_pools=num_pools,
                                              maxsize=maxsize,
-                                             timeout=10,
+                                             timeout=40,
                                              retries=urllib3.util.retry.Retry(15))
 
         return self.http_pool
