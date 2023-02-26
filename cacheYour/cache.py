a03be34729a6a8c0c7a96e6641e079826990ec54
thijmen francken <thijmenfrancken@gmail.com>
2023-02-26 14:33:21 +0100

    cache class error fix

--- cacheYour/cache.py
+++ cacheYour/cache.py
@@ -1,7 +1,3 @@
-from cacheYour.attributes.indexAttributes import checkAttributeStatusCache as checkAttributeIndexStatusCache, processIndexAttributeCache
-from cacheYour.brands.brand import checkBrandStatusCache, setBrandCache
-from cacheYour.categories.category import checkCategoryStatusCache, setCategoryCache
-from cacheYour.series.serie import checkSeriesStatusCache, processSeriesCache
 from cacheYour.client import RedisClient
 
 class DataCache:
@@ -71,12 +67,9 @@ class DataCache:
             attribute_cache = AttributeIndex(client=self.client)
             attribute_cache.processIndexAttributeCache(index_attributes=index_attributes)
             print(f"Index Attribute Cache processed")
-
-        ## attributes logic
-        if index_attributes:
-            processIndexAttributeCache(index_attributes=index_attributes)
         else:
             from cacheYour.attributes.attribute import AttributeCache
             attribute_cache = AttributeCache(client=self.client)
             attribute_cache.processAttributeCache()
             attribute_cache.processAttributeValueUnitCache()
+            print(f"Index Attribute Cache processed")
