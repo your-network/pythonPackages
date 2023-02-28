a9229c6d2a3cdacf5d38256ca5182a38a9b9d887
thijmen francken <thijmenfrancken@gmail.com>
2023-02-28 11:18:58 +0100

    testing new cache client.py

--- cacheYour/client.py
+++ cacheYour/client.py
@@ -1,33 +1,20 @@
 import redis
 
-class Singleton(type):
-    """
-    An metaclass for singleton purpose. Every singleton class should inherit from this class by 'metaclass=Singleton'.
-    """
-    _instances = {}
+def getRedisPool():
+    print(f"Creating 1000 connection, aleksei wants to see this")
+    pool = redis.ConnectionPool(host="localhost",
+                                     port=6379,
+                                     db=0,
+                                     max_connections=10000)
+    return pool
 
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-class RedisPool(metaclass=Singleton):
-
-    def getPool(self):
-        print(f"Creating 1000 connection, aleksei wants to see this")
-        self.pool = redis.ConnectionPool(host="localhost",
-                                         port=6379,
-                                         db=0,
-                                         max_connections=10000)
-        return self.pool
-
-class RedisClient(metaclass=Singleton):
+class RedisClient:
     def __init__(self, pool):
         self.pool = pool
 
     def getConnection(self):
         self.conn = redis.Redis(connection_pool=self.pool,
-                                 socket_timeout=300)
+                                socket_timeout=300)
         return self.conn
 
     def closeConnection(self):
